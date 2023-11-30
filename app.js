ee5ea968008cca817d33094e44f56018d3d7d254
joebakid <josephbawo@gmail.com>
2023-11-30 22:17:54 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,56 @@
+// Set the default tab to "DRC" on page load
+document.addEventListener("DOMContentLoaded", function () {
+  openTab("drc");
+});
+
+function openTab(tabName) {
+  // Hide all tab contents
+  var tabContents = document.querySelectorAll(".tab-content");
+  tabContents.forEach(function (content) {
+    content.style.display = "none";
+  });
+
+  // Deactivate all tabs
+  var tabs = document.querySelectorAll(".tab");
+  tabs.forEach(function (tab) {
+    tab.classList.remove("active");
+  });
+
+  // Show the selected tab content
+  document.getElementById(tabName + "Content").style.display = "block";
+
+  // Activate the selected tab
+  document
+    .querySelector('.tab[data-tab="' + tabName + '"]')
+    .classList.add("active");
+}
+
+const blogLinks = document.querySelectorAll(".blog-page");
+
+blogLinks.forEach(function (blogLink) {
+  blogLink.addEventListener("click", function () {
+    document.querySelector("body").style.transform = "scale(0.9)";
+    document.querySelector("body").style.opacity = "0";
+  });
+});
+
+const main = document.querySelector(".main");
+
+document.addEventListener("DOMContentLoaded", function () {
+  gsap.fromTo(
+    ".p-main",
+    {
+      opacity: 0,
+      backgroundColor: "gray",
+      color: "gray",
+      delay: 0.7,
+    },
+    {
+      opacity: 1,
+      backgroundColor: "white",
+      color: "black",
+      ease: "back.out(1.7)",
+      duration: 10,
+    }
+  );
+});
